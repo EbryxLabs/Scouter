b3a42e91a2587578f2e306d44a27555c49ba6fe3
Ali Ahmed Dar <ali.ahmed.external@moonfare.com>
2024-03-17 15:28:38 +0500

    Test for mysql flexible server

--- Azure/MySQL/azure_mysql_ssl_encrpyted_connection.py
+++ Azure/MySQL/azure_mysql_ssl_encrpyted_connection.py
@@ -40,11 +40,11 @@ def azure_mysql_ssl_encrpyted_connection():
         for group in list(group_list):
             resource_group_name = group.name
             
-            # Define the request to fetch all SQL servers from each resource group
-            diagnostic_settings_endpoint = f"https://management.azure.com/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.DBforMySQL/flexibleServers?api-version=2021-05-01"
+            # Define the request to fetch all individual SQL servers from each resource group
+            diagnostic_settings_endpoint = f"https://management.azure.com/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.DBforMySQL/servers?api-version=2021-05-01"
             headers = {"Authorization": f"Bearer {credentials.get_token('https://management.azure.com').token}"}
             response = requests.get(diagnostic_settings_endpoint, headers=headers)
-
+            
             if response.status_code == 200:
                 data = response.json()
 
@@ -52,7 +52,7 @@ def azure_mysql_ssl_encrpyted_connection():
                     server_id = item.get("id",{})
                     server_name = item.get("name",{})
 
-                    diagnostic_settings_endpoint = f"https://management.azure.com/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.DBforMySQL/flexibleServers/{server_name}/configurations?api-version=2021-05-01"
+                    diagnostic_settings_endpoint = f"https://management.azure.com/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.DBforMySQL/servers/{server_name}/configurations?api-version=2017-12-01"
                     headers = {"Authorization": f"Bearer {credentials.get_token('https://management.azure.com').token}"}
                     response = requests.get(diagnostic_settings_endpoint, headers=headers)
                     data = response.json()
@@ -61,7 +61,27 @@ def azure_mysql_ssl_encrpyted_connection():
                         if item["name"] == "require_secure_transport":
                             result_list.append({f"SSL connection requirement results for {server_name}: ":item})
 
+            
+            # Define the request to fetch all flexible SQL servers from each resource group
+            diagnostic_settings_endpoint = f"https://management.azure.com/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.DBforMySQL/flexibleServers?api-version=2021-05-01"
+            headers = {"Authorization": f"Bearer {credentials.get_token('https://management.azure.com').token}"}
+            response = requests.get(diagnostic_settings_endpoint, headers=headers)
+            if response.status_code == 200:
+                data = response.json()
 
+                for item in data.get("value",[]):
+                    server_id = item.get("id",{})
+                    server_name = item.get("name",{})
+
+                    diagnostic_settings_endpoint = f"https://management.azure.com/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers/Microsoft.DBforMySQL/flexibleServers/{server_name}/configurations?api-version=2023-06-30"
+                    headers = {"Authorization": f"Bearer {credentials.get_token('https://management.azure.com').token}"}
+                    response = requests.get(diagnostic_settings_endpoint, headers=headers)
+
+                    data = response.json()
+
+                    for item in data.get("value",[]):
+                        if item["name"] == "require_secure_transport":
+                            result_list.append({f"SSL connection requirement results for {server_name}: ":item})
 
     with open(output_file, 'w') as outfile:
         json.dump(result_list, outfile, indent=4)
